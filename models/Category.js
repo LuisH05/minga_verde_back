95c2d1f0bcb9bbbb3b3957b71206feed632c42ee
Luis Hernandez <125385157+LuisH05@users.noreply.github.com>
2023-04-21 14:30:39 -0500

    b-05 (#4)

---
+++ models/Category.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+
+let schema = new mongoose.Schema({
+    name: {type:String, required:true},
+    color: {type:String, required:true},
+    hover: {type:String, required:true},
+    description: {type:String, required:true},
+    cover_photo: {type:String, required:true},
+    charancter_photo: {type:String, required:true},
+},{
+    timestamps: true
+})
+let collection = 'categories'
+
+let Category = mongoose.model(schema,collection)
+
+export default Category
