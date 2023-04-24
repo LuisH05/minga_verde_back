61d7a179b2d0141015c364687593cc9af624ac55
Luis Hernandez <125385157+LuisH05@users.noreply.github.com>
2023-04-24 12:52:39 -0500

    M 02 (#13)
    * main
    
    * m-02
    
    * m-02

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-
+import cors from 'cors'
 import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
 import { __dirname } from './utils.js';
@@ -15,7 +15,7 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
